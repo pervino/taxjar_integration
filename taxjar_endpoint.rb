6de0a9d0a8ea16dafe8ad27a6dc277bc8093062b
Anthony D'Addeo <amdaddeo@gmail.com>
2016-02-12 14:44:14 -0600

    Serve static files

--- taxjar_endpoint.rb
+++ taxjar_endpoint.rb
@@ -2,6 +2,7 @@ require_relative 'lib/taxjar_integration'
 
 class TaxjarEndpoint < EndpointBase::Sinatra::Base
   set :logging, true
+  set :public, 'public'
 
   post '/add_or_update_order' do
     code, summary = TaxjarIntegration::Order.new(@config, @paylod).add_or_update
